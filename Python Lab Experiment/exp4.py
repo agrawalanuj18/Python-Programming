8724adbf1f76aa9157bf97002eed167fa25e187e
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-09-16 23:15:24 +0530

    add

---
+++ Python Lab Experiment/exp4.py
@@ -0,0 +1,26 @@
+# Taken day number from user
+weekday = int(input("Enter weekday day number (1-7) : "))
+
+if weekday == 1:
+    print("Monday")
+
+elif weekday == 2:
+    print("Tuesday")
+
+elif (weekday == 3):
+    print("Wednesday")
+
+elif (weekday == 4):
+    print("Thursday")
+
+elif (weekday == 5):
+    print("Friday")
+
+elif (weekday == 6):
+    print("Saturday")
+
+elif (weekday == 7):
+    print("Sunday")
+
+else:
+    print("\nPlease enter weekday number between 1-7.")
